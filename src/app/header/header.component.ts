14547a5fa9eb5d50e14edcd3b8162a74466e662b
vaibhavshukla-dev <shukla.vaibhav24@gmail.com>
2020-10-08 02:29:48 +0530

    Update in home page and Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,11 +12,25 @@ export class HeaderComponent implements OnInit {
 
 
   loggedinUser: string = localStorage.getItem('loggedinuser') || "";
+  isAdmin: string = localStorage.getItem('isAdmin') || "";
   constructor(private authService: AuthService, private router: Router, private utilityService: UtilityService) {  }
 
+
+  navArray = [
+    { title: "Home", url: "/dashboard", type: "1", icon: "dashboard"},
+    { title: "Accounts", url: "/accounts", type: "1", icon: "receipt"},
+    { title: "Contacts", url: "/contacts", type: "1", icon: "contacts"},
+    { title: "Requests", url: "/loanapplications", type: "1", icon: " move_to_inbox"},
+    { title: "Scripts", url: "/scripts", type: "1", icon: "wysiwyg"},
+    { title: "Communicate", url: "/communicates", type: "1", icon: "textsms"},
+    { title: "Reports", url: "/reports", type: "1", icon: "assessment"}
+  ];
+
   ngOnInit() {
-    
+
   }
+  
+
   logout(event){
     this.authService.logout();
     this.router.navigate(["login"]);
